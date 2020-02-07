73134a3809f7a5f740af5806a13347c37438bdae
Manuel Duro <mduro@edu.tecnocampus.cat>
2020-02-08 00:29:11 +0100

    First commit

---
+++ src/main/java/com/example/hackathonbaldragas/HackathonBaldragasApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackathonbaldragas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonBaldragasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonBaldragasApplication.class, args);
+    }
+
+}
