7322d3051c8b5ee7134fd0c703d0ca37b46ffbba
Pol Fernàndez <pfernadezgu@edu.tecnocampus.cat>
2020-02-08 01:56:52 +0100

    User categories implemented

--- src/main/java/com/example/hackathonbaldragas/persistence/UserDAO.java
+++ src/main/java/com/example/hackathonbaldragas/persistence/UserDAO.java
@@ -16,7 +16,12 @@ public class UserDAO {
     private JdbcTemplate jdbcTemplate;
 
     private final String FIND_ALL = "select * from \"USER\"";
-    private final String INSERT = "insert into \"USER\" (dni, password, name, surnames, birthday, mail, phone, address, availability, senior) values(?,?,?,?,?,?,?,?,?,?)"; //falta usercateogry(?)
+    private final String INSERT = "insert into \"USER\" (dni, password, name, surnames, birthday, mail, phone, address, availability, senior) values(?,?,?,?,?,?,?,?,?,?)";
+
+    //CATEGORIES
+    private final String FIND_USER_CATEGORIES = "select * from \"USER_CATEGORY\" where user_dni = ?";
+    private final String INSERT_USER_CATEGORY = "insert into \"USER_CATEGORY\"  (user_dni, category_name) values(?, ?)";
+    private final String DELETE_USER_CATEGORY = "delete from \"USER_CATEGORY\"  where user_dni = ? and category_name = ?";
 
 
 
@@ -32,17 +37,13 @@ public class UserDAO {
                 .address(resultSet.getString("address"))
                 .availability(resultSet.getString("availability"))
                 .senior(resultSet.getBoolean("senior"))
-                //.likesGender(getUserLikes(resultSet.getString("username")))
+                .categoriesList(getUserCategories(resultSet.getString("dni")))
                 .build();
     };
 
-    /*private final RowMapper<Integer> likesMapper = (resultSet, i) -> {
-        return resultSet.getInt("gender_id");
-    };
-
-    private final RowMapper<String> interestsMapper = (resultSet, i) -> {
+    private final RowMapper<String> categoriesMapper = (resultSet, i) -> {
         return resultSet.getString("category_name");
-    };*/
+    };
 
     public UserDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -52,12 +53,22 @@ public class UserDAO {
         return jdbcTemplate.query(FIND_ALL, new BeanPropertyRowMapper<>(User.class));
     }
 
-    //Falta el tema de user_category
+    public List<String> getUserCategories(String dni) {
+        return jdbcTemplate.query(FIND_USER_CATEGORIES, new Object[]{dni}, categoriesMapper);
+    }
+
     public int insert(User user) {
         return jdbcTemplate.update(INSERT, user.getDni(), user.getPassword(), user.getName(), user.getSurnames(),
                 Date.valueOf(user.getBirthday()), user.getMail(), user.getPhone(), user.getAddress(), user.getAvailability(), user.getSenior());
     }
 
+    public int insertUserCategory(String dni, String categoryName) {
+        return jdbcTemplate.update(INSERT_USER_CATEGORY, dni, categoryName);
+    }
+
+    public int deleteUserCategory(String dni, String categoryName) {
+        return jdbcTemplate.update(DELETE_USER_CATEGORY, dni, categoryName);
+    }
 
 
 }
