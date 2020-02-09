51c6624067a9a7425b4aea1a2915ce8ed95ae01e
Alejandro <acostar@edu.tecnocampus.cat>
2020-02-09 01:12:17 +0100

    More html

--- src/main/java/com/example/hackathonbaldragas/controller/ControllerDAO.java
+++ src/main/java/com/example/hackathonbaldragas/controller/ControllerDAO.java
@@ -41,63 +41,13 @@ public class ControllerDAO {
         return userDAO.findByName(name);
     }
 
-    /*public User findUserByDni(String dni) { return userDAO.findByDni(dni).get(0); }
-    public List<Category> findUserCategories(String dni) { return userDAO.getUserCategories(dni); }
-    public List<User> findUserByFilter(UserFilter filter) { return userDAO.findByFilter(filter); }
-    //inserts
-    public int insertUser(User user) { return userDAO.insert(user); }
-
-    public int insertUserCategory(String dni, String category) { return userDAO.insertUserCategory(dni, category); }
-    //deletes
-    public int deleteUser(User user) { return userDAO.delete(user); } //probably not even necessary. oh well.
-    public int deleteUserCategory(String dni, String category) { return userDAO.deleteUserCategory(dni, category); }
-
-    //CATEGORY
-
-    public List<Category> findAllCategories() {
-        return categoryDAO.findAll();
-    }
-
-    public int updateUser(User user) { return userDAO.update(user); }
-
-
-
-    //REQUEST
-
-    public int insertRequest(Request request){
-        return requestDAO.insert(request);
-    }
-
-    public List<Request> findAllRequests() {
-        return requestDAO.findAll();
-    }
-
-    public Request findRequest(int id) {
-        return requestDAO.find(id);
-    }
-
-    public String getRequestCreator(int id){
-        return requestDAO.getUser(id);
-    }
-
-
-    //LINK
-
-    public List<Link> findAllLinks() {
-        return linkDAO.findAll();
-    }
-    public int insertLink(Link link){return linkDAO.insert(link);}
-    public int updateLink(Link link){return linkDAO.update(link);}
-    public int insertNewLink(Link link){return linkDAO.insertNew(link);}
-    public int deleteLink(Link link){return linkDAO.delete(link);}*/
-
     //ACTIVITY
     public List<Activity> findAllActivities() { return activityDAO.findAll(); }
     public List<Activity> findActivitiesByUser(String mail) { return activityDAO.findByUser(mail); }
     public List<Activity> getActivityBetweenDates(String mail, LocalDate datea, LocalDate dateb) { return activityDAO.findBetweenDates(mail, datea, dateb); }
 
     //MILESTONE
-    public int insertMilestone(Milestone milestone) { return milestoneDAO.insert(milestone); }
+    public int insertMileStone(Milestone milestone) { return milestoneDAO.insert(milestone); }
     public List<Milestone> findAllMilestones() { return milestoneDAO.findAll(); }
     public List<Milestone> findMilestonesByUser(String userMail) {
         List<Milestone> result = milestoneDAO.findMilestonesByUser(userMail);
